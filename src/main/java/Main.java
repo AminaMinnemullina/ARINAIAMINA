7f26c7c36dd415ba2df802f9b5916df8d0cfab8c
Arina <arina.klushnik@gmail.com>
2025-11-29 12:24:14 +0500

    create message and user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
 public class Main {
+    private static final List<Message> messages = new ArrayList<>();
+    public static void main(String[] args) {
+
+        User[] users = new User[4];
+        users[0] = new User("Амина", "666");
+        users[1] = new User("Арина", "007");
+        users[2] = new User("Богдаша", "123");
+        users[3] = new User("Айгизя", "456");
+
+        System.out.println("Пользователи: ");
+        for (User user : users) {
+            System.out.println(user.toString());
+
+            sendMessage(users[0], users[1], "Привет!");
+            sendMessage(users[2], users[3], "Как дела?");
+            sendMessage(users[1], users[0], "Что задали по проге?");
+
+        }
+    }
+    public static void sendMessage(User fromUser, User toUser, String text) {
+        Message message = new Message(fromUser, toUser, text);
+        messages.add(message);
+    }
 }
+
